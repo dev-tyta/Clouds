0dcb0e4fc6845e5a36d60fd923f9a78988ed6d3d
dev-tyta <hardeykoryar7@gmail.com>
2022-12-26 08:35:52 +0100

    test output works perfectly well

--- main.py
+++ main.py
@@ -2,16 +2,19 @@
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 
+# receives input from user
 into = input("Put in a sentence: ")
 
 
+# function to generate cloud for the sentence
 def create(inp):
     word = WordCloud().generate(inp)
     return word
 
 
+# call function to generate cloud for sentence from the user.
 ret_word = create(into)
-plt.figure()
+plt.figure()    # creates figure for matplotlib to display.
 plt.imshow(ret_word, interpolation="bilinear")
 plt.axis("off")
 plt.show()
