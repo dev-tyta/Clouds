7630db1be4d56945fc30fea007f79878bf79f419
dev-tyta <hardeykoryar7@gmail.com>
2022-12-26 08:27:03 +0100

    testing library functionality

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+# importing necessary libraries
 from wordcloud import WordCloud
+import matplotlib.pyplot as plt
 
-def create(input):
+into = input("Put in a sentence: ")
+
+
+def create(inp):
+    word = WordCloud().generate(inp)
+    return word
+
+
