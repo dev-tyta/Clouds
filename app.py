a7f29d07ccd995779f9aaf830d75933f0191f787
dev-tyta <hardeykoryar7@gmail.com>
2022-12-29 22:23:20 +0100

    gradio

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import gradio as gr
 
 
 def greet(name):
-    return f"Hello {name} !"
+    return f"Hello {name}! Welcome to our test page!"
 
 
 demo = gr.Interface(fn=greet, inputs="text", outputs="text")
